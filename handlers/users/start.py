6b0c37e3ce75150418973e5f52bae6806e0279f3
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-26 17:05:50 +0500

    feat: command start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,8 +3,15 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
 
+dp.message_handler(CommandStart(deep_link='kunuz'))
+async def bot_start(message: types.message):
+    args = message.get_args()
+    text = f"Salom, {message.from_user.full_name}!\n"
+    text += f"Sizni {args} tavsiya qildi"
+    await message.answer(text)
+    
 
 @dp.message_handler(commands='start') # bu ko'rinishda istalgan command uchun yozishimiz mumkin
-@dp.message_handler(CommandStart())
+@dp.message_handler(CommandStart(deep_link='kunuz')) # bu handler aynan kunuzdan kirgan va start bosgan userlarni qabul qilib oladi
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
