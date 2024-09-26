7840f926e720be27676ac395eba196a7aee3b33f
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-26 13:13:36 +0500

    feat: command handler

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,6 +4,7 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 
 
+@dp.message_handler(commands='start') # bu ko'rinishda istalgan command uchun yozishimiz mumkin
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
