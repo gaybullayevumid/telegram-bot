582c28d0de5264ca3878172f53dafc533fa7387c
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-09-27 13:27:02 +0500

    feat: create more content handlers

--- handlers/users/kontenthandlers.py
+++ handlers/users/kontenthandlers.py
@@ -2,11 +2,25 @@ from aiogram import types
 from aiogram.dispatcher import filters
 from loader import dp
 
-
+@dp.message_handler(content_types='document')
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def photo_handler(msg: types.Message):
-    await msg.answer('Bu nima rasm?')
+    await msg.answer('Bu nima?')
 
-@dp.message_handler(content_types='photo')
-async def photo_handler(msg: types.Message):
-    await msg.answer('Nima rasm bu?')
+# @dp.message_handler(content_types='photo')
+# async def photo_handler(msg: types.Message):
+#     await msg.answer('Nima rasm bu?')
+
+@dp.message_handler(content_types='sticker')
+async def emoji_handler(msg: types.Message):
+    await msg.answer('😂')
+
+
+@dp.message_handler(content_types='contact')
+async def contact_handler(msg: types.Message):
+    await msg.answer('Kim bu odam?')
+
+
+@dp.message_handler(content_types=types.ContentType.VOICE)
+async def audio_handler(msg: types.Message):
+    await msg.answer('Yaxshi eshitmadim')
